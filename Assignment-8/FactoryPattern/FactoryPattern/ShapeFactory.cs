3f1a26151d7ff23e15bf89fed95976dfab8e1212
Shubhangi Pandey <77426618+Shubhangi30-cloud@users.noreply.github.com>
2023-09-27 00:20:22 +0530

    Add files via upload

---
+++ Assignment-8/FactoryPattern/FactoryPattern/ShapeFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public class ShapeFactory
+    {
+        public static IShape GetShape(string shapeType)
+        {
+            IShape Icc = null;
+            if (shapeType == "Triangle")
+            {
+                Icc = new Triangle();
+            }
+            else if (shapeType == "Rectangle")
+            {
+                Icc = new Rectangle();
+            }
+            else if (shapeType == "Circle")
+            {
+                Icc = new Circle();
+            }
+            return Icc;
+        }
+    }
+}
