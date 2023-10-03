43c3fcbb5dcebe7ba79bcbdaf15fc3213995a82a
Shubhangi Pandey <shubhangp@infinite.com>
2023-10-03 18:06:26 +0530

    Test_4

---
+++ CodeBasedTest/CodeBasedTest-4/CodeBasedTest-4/Calculator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBasedTest_4
+{
+    public delegate int CalculatorDelegate(int num1, int num2);
+
+
+
+    class Calculator
+
+    {
+
+        static int Add(int num1, int num2)
+
+        {
+            return num1 + num2;
+        }
+
+        static int Subtract(int num1, int num2)
+
+        {
+            return num1 - num2;
+        }
+
+        static int Multiply(int num1, int num2)
+
+        {
+            return num1 * num2;
+        }
+
+        static void Main()
+
+        {
+            Console.WriteLine("Enter two numbers:");
+
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+            CalculatorDelegate addDelegate = new CalculatorDelegate(Add);
+
+            CalculatorDelegate subtractDelegate = new CalculatorDelegate(Subtract);
+
+            CalculatorDelegate multiplyDelegate = new CalculatorDelegate(Multiply);
+
+
+
+            int resultAddition = addDelegate(num1, num2);
+
+            int resultSubtraction = subtractDelegate(num1, num2);
+
+            int resultMultiplication = multiplyDelegate(num1, num2);
+
+
+            Console.WriteLine($"Addition: {resultAddition}");
+            Console.WriteLine($"Subtraction: {resultSubtraction}");
+            Console.WriteLine($"Multiplication: {resultMultiplication}");
+            Console.ReadLine();
+        }
+        
+
+    }
+}
---
+++ CodeBasedTest/CodeBasedTest-4/CodeBasedTest-4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBasedTest_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            string filePath = "C:\\Users\\shubhangp\\desktop\\sample.txt";
+            Console.WriteLine("Enter text to append to the file:");
+            string textToAppend = Console.ReadLine();
+
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    File.Create(filePath).Close();
+                }
+                using (StreamWriter writer = new StreamWriter(filePath, true))
+
+                {
+                    writer.WriteLine(textToAppend);
+                }
+                Console.WriteLine("Text appended to the file successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+
+}
