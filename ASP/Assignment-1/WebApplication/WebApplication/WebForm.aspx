7ecbbc9e1093429f1c78c2fbc69569ec4cbda5a8
Shubhangi Pandey <shubhangp@infinite.com>
2023-10-31 16:05:05 +0530

    ASP Assignment

---
+++ ASP/Assignment-1/WebApplication/WebApplication/WebForm.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm.aspx.cs" Inherits="WebApplication.WebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h1>Welcome to chocolate Store</h1>
+        
+        <label for="ddlItems">Select an Item:</label>
+        <asp:DropDownList ID="ddlItems" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlItems_SelectedIndexChanged">
+            <asp:ListItem Text="Select an item" Value="" />
+            <asp:ListItem Text="Dairy Milk" Value="DairyMilk.jpg" />
+            <asp:ListItem Text="Kitkat" Value="Kitkat.jpg" />
+            <asp:ListItem Text="5 Star" Value="5 Star.jpg" />
+            <asp:ListItem Text="Perk" Value="Perk.jpg" />
+            <asp:ListItem Text="Munch" Value="Munch.jpg" />
+        </asp:DropDownList>
+        <br />
+
+        <asp:Image ID="imgItem" runat="server" AlternateText="Item Image" CssClass="item-image" Height="240px" Width="296px" />
+        <br />
+
+        <asp:Button ID="btnShowCost" runat="server" Text="Show Cost" OnClick="btnShowCost_Click" />
+        <br />
+
+        <asp:Label ID="lblCost" runat="server" Text=""></asp:Label>
+    </form>
+</body>
+</html>
---
+++ ASP/Assignment-1/WebApplication/WebApplication/WebForm.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication
+{
+    public partial class WebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Set the initial selection of the dropdown list
+                ddlItems.SelectedIndex = 0;
+                // Display the default image (you can set this in the markup as well)
+                imgItem.ImageUrl = "~/Images/default.jpg";
+            }
+        }
+
+        protected void ddlItems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Set the image URL based on the selected item
+            string selectedImageUrl = ddlItems.SelectedValue;
+            imgItem.ImageUrl = $"~/Images/{selectedImageUrl}";
+            lblCost.Text = ""; // Clear the cost label.
+        }
+
+        protected void btnShowCost_Click(object sender, EventArgs e)
+        {
+            string selectedItem = ddlItems.SelectedValue;
+
+            // Define a dictionary of item costs (you can use a database in a real application).
+            var itemCosts = new Dictionary<string, decimal>
+            {
+                { "DairyMilk.jpg", 200.00m },
+                { "Kitkat.jpg", 150.00m },
+                { "Munch.jpg", 100.00m },
+                { "Perk.jpg", 120.00m },
+                {"5 Star.jpg", 20.00m },
+            };
+
+            if (itemCosts.ContainsKey(selectedItem))
+            {
+                lblCost.Text = $"Cost: ${itemCosts[selectedItem]}";
+            }
+            else
+            {
+                lblCost.Text = "Cost not available.";
+            }
+        }
+    }
+}
